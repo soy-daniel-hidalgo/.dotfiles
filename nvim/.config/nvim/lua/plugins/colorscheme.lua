2b0b6fbe62b267924bb961b68ffad24fc27fa42b
soy-daniel-hidalgo <contact@danielhidalgo.dev>
2026-08-15 03:23:18 +0200

    feat(nvim): apply catppuccin color scheme to LazyVim

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,34 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "mocha", -- latte, frappe, macchiato, mocha
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
+      transparent_background = true,
+      show_end_of_buffer = false,
+      integration_default = true,
+      integrations = {
+        cmp = true,
+        gitsigns = true,
+        nvimtree = true,
+        treesitter = true,
+        notify = true,
+        mini = {
+          enabled = true,
+          indentscope = true,
+        },
+      },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
