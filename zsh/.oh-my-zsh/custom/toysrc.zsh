d79115128bdce696fd5dafa5162bb523bf70f324
soy-daniel-hidalgo <contact@danielhidalgo.dev>
2026-08-15 16:52:40 +0200

    Rename cli-toys to cli-extras and remove distractions from workflow

--- zsh/.oh-my-zsh/custom/toysrc.zsh
+++ zsh/.oh-my-zsh/custom/toysrc.zsh
@@ -1,27 +1,5 @@
-# bastet
-alias tetris=bastet
-
-# asciiquarium
-alias aqua=asciiquarium
-
-# cbonsai tree
-alias bonsai=cbonsai
-
-# screensavers
-alias matrix="cmatrix -s -b"
-
 # colorscripts
-alias bloks="source ~/.scripts/colorscripts/bloks"
-alias colorbars="source ~/.scripts/colorscripts/colorbars"
-alias colortest="source ~/.scripts/colorscripts/colortest"
-alias colortest-slim="source ~/.scripts/colorscripts/colortest-slim"
-alias colorview="source ~/.scripts/colorscripts/colorview"
-
-# steam locomotive
-alias train="sl -e"
-alias liltrain="sl -le"
-alias flytrain="sl -alFe"
-alias loltrain="sl -e | lolcat"
+alias panes="source ~/.scripts/colorscripts/panes"
 
 # display smaller neofetch-like system info
 alias jfetch="source ~/.scripts/jfetch"
