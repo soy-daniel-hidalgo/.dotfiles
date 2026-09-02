59077fb7372954f34aea4c417cafde2306c9be54
soy-daniel-hidalgo <contact@danielhidalgo.dev>
2026-09-02 22:48:54 +0200

    style(nvim): expand catppuccin colorscheme plugin configuration

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -5,23 +5,71 @@ return {
     priority = 1000,
     opts = {
       flavour = "mocha", -- latte, frappe, macchiato, mocha
-      background = {
+      background = { -- :h background
         light = "latte",
         dark = "mocha",
       },
-      transparent_background = true,
-      show_end_of_buffer = false,
-      integration_default = true,
+      transparent_background = true, -- disables setting the background color.
+      float = {
+        transparent = true, -- enable transparent floating windows
+        solid = false, -- use solid styling for floating windows, see |winborder|
+      },
+      term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+      dim_inactive = {
+        enabled = false, -- dims the background color of inactive window
+        shade = "dark",
+        percentage = 0.15, -- percentage of the shade to apply to the inactive window
+      },
+      no_italic = false, -- Force no italic
+      no_bold = false, -- Force no bold
+      no_underline = false, -- Force no underline
+      styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+        comments = { "italic" }, -- Change the style of comments
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+        -- miscs = {}, -- Uncomment to turn off hard-coded styles
+      },
+      lsp_styles = { -- Handles the style of specific lsp hl groups (see `:h lsp-highlight`).
+        virtual_text = {
+          errors = { "italic" },
+          hints = { "italic" },
+          warnings = { "italic" },
+          information = { "italic" },
+          ok = { "italic" },
+        },
+        underlines = {
+          errors = { "underline" },
+          hints = { "underline" },
+          warnings = { "underline" },
+          information = { "underline" },
+          ok = { "underline" },
+        },
+        inlay_hints = {
+          background = true,
+        },
+      },
+      color_overrides = {},
+      custom_highlights = {},
+      auto_integrations = true,
       integrations = {
         cmp = true,
         gitsigns = true,
         nvimtree = true,
-        treesitter = true,
-        notify = true,
+        notify = false,
         mini = {
           enabled = true,
-          indentscope = true,
+          indentscope_color = "",
         },
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
       },
     },
   },
