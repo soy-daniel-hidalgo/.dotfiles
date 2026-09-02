c691bca81b8b65853db2df5de073d77cea28c948
soy-daniel-hidalgo <contact@danielhidalgo.dev>
2026-09-03 00:19:52 +0200

    style(zsh): normalize comment syntax in custom aliases

--- zsh/.oh-my-zsh/custom/aliases.zsh
+++ zsh/.oh-my-zsh/custom/aliases.zsh
@@ -1,40 +1,40 @@
 # Package manager dnf5
 
-## Update dependencies
+# Update dependencies
 alias update="sudo dnf5 update"
 
-## Autoremove unneeded packages
+# Autoremove unneeded packages
 alias autoremove="sudo dnf5 autoremove"
 
-## List installed packages
+# List installed packages
 alias installed="dnf5 list --installed | fzf"
 
-# Quick navigation
+# Quick access
 
-## Images
+# Images
 alias pics="cd ~/Pictures"
 
-## Videos
+# Videos
 alias videos="cd ~/Videos"
 
-## Downloads
+# Downloads
 alias downloads="cd ~/Downloads"
 
-## Documents
+# Documents
 alias docs="cd ~/Documents"
 
-## Desktop
+# Desktop
 alias desk="cd ~/Desktop"
 
-## Music
+# Music
 alias music="cd ~/Music"
 
-## Applications
+# Applications
 alias apps="cd ~/Applications"
 
-# CLI reconnaissance
+# CLI navigation
 
-## eza
+# eza
 alias ls="eza --icons --color=always --git --no-filesize --no-time --no-user --no-permissions"
 
 alias la="eza --icons -lgha --group-directories-first"
@@ -43,67 +43,67 @@ alias lt="eza --icons --tree"
 
 alias lta="eza --icons --tree -lgha"
 
-## batcat
+# batcat
 alias cat='bat --theme=$BAT_THEME --style=plain --paging=never $@'
 
 alias bat='bat --theme=$BAT_THEME'
 
-## fzf
+# fzf
 alias preview='fzf --preview "batcat --color=always --style=numbers --line-range=:500 {}"'
 
 alias edit='nvim $(preview)'
 
-## Large files on disk
+# Large files on disk
 alias largefile="du -h -x -s -- * | sort -r -h | head -20"
 
 # Utilities
 
-## Search manual
+# Search manual
 alias findman="compgen -c | fzf | sort | xargs man"
 
-## Search environment variables
+# Search environment variables
 alias getenv="printenv | sort | fzf"
 
-## Search aliases
+# Search aliases
 alias getalias="alias | sort | fzf"
 
-## Reload .zshrc
+# Reload .zshrc
 alias reload="source ~/.zshrc"
 
-## Unsource shell
+# Unsource shell
 alias unsource="exec $SHELL -l"
 
 # Source control
 
-## Print git-log as graph
+# Print git-log as graph
 alias gitgraph="git log --graph --decorate --abbrev-commit --all --pretty=oneline"
 
 # Edit config files
 
-## bashrc
+# bashrc
 alias bashconf="nvim ~/.bashrc"
 
-## zshrc
+# zshrc
 alias zshconf="nvim ~/.zshrc"
 
-## zshenv
+# zshenv
 alias zshenv="nvim ~/.zshenv"
 
-## autostart
+# autostart
 alias autostart="nvim $ZSH/custom/autostart.zsh"
 
-## Check my aliases
+# Check my aliases
 alias aliases="nvim $ZSH/custom/aliases.zsh"
 
-## CLI toys
+# CLI toys
 alias toysconf="nvim $ZSH/custom/toysrc.zsh"
 
-## Oh-My-Zsh config directory
+# Oh-My-Zsh config directory
 alias omzconf="nvim $ZSH"
 
-## Neovim config directory with lazyvim setup
+# Neovim config directory with lazyvim setup
 alias nvimconf="nvim $XDG_CONFIG_HOME/nvim"
 
-## tmux
+# tmux
 alias tmuxconf="nvim $XDG_CONFIG_HOME/tmux/tmux.conf"
 
