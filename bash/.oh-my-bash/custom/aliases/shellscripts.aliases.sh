f5920705410d618b2e084fca1c09d49d37f9ccd1
soy-daniel-hidalgo <contact@danielhidalgo.dev>
2026-09-07 13:30:44 +0200

    refactor: migrate Zsh and Bash shellscripts aliases to XDG directory structure

--- bash/.oh-my-bash/custom/aliases/shellscripts.aliases.sh
+++ bash/.oh-my-bash/custom/aliases/shellscripts.aliases.sh
@@ -1,9 +1,9 @@
 # colorscripts
-alias panes="source ~/.local/bin/panes"
-alias crunchbang-mini="source ~/.local/bin/crunchbang-mini"
+alias panes="source $XDG_BIN_HOME/panes"
+alias crunchbang-mini="source $XDG_BIN_HOME/crunchbang-mini"
 
 # display smaller neofetch-like system info
-alias jfetch="source ~/.local/bin/jfetch"
+alias jfetch="source $XDG_BIN_HOME/jfetch"
 
 # local weather
 weatherin() { curl -s wttr.in/"$1"; }
