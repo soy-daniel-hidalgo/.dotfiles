a24190a3899379f45df75b2820646d8e4173a019
soy-daniel-hidalgo <contact@danielhidalgo.dev>
2026-09-09 13:36:27 +0200

    feat: add crunchbang-mini as alias for bash and zsh

--- zsh/.oh-my-zsh/custom/shellscripts.zsh
+++ zsh/.oh-my-zsh/custom/shellscripts.zsh
@@ -1,5 +1,6 @@
 # colorscripts
 alias panes="source ~/.scripts/colorscripts/panes"
+alias crunchbang-mini="source ~/.scripts/colorscripts/crunchbang-mini"
 
 # display smaller neofetch-like system info
 alias jfetch="source ~/.scripts/jfetch"
